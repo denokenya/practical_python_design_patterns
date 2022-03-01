4398298c720a50671440e85673696ade3243bb4c
denokenya <dennismuteti25@gmail.com>
2022-03-01 14:40:36 +0300

    code refactor

--- chapter_3/rts_simple.py
+++ chapter_3/rts_simple.py
@@ -69,9 +69,15 @@ class Barracks(object):
 
         return Knight(400, 5, 3, 2, "short sword")
 
+    def generate_archer(self):
+
+        return Archer(200, 7, 1, 5, "short bow")
+
 
 if __name__ == "__main__":
 
     barracks = Barracks()
     knight1 = barracks.generate_knight()
+    archer1 = barracks.generate_archer()
     print("[knight1] {}".format(knight1))
+    print("[archer1] {}".format(archer1))

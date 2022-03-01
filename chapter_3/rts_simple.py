473643417728fec341a8883a8502a38ded6552fa
denokenya <dennismuteti25@gmail.com>
2022-03-01 14:13:17 +0300

    code refactor

---
+++ chapter_3/rts_simple.py
@@ -0,0 +1,29 @@
+class Knight(object):
+
+    def __init__(
+        self,
+        life,
+        speed,
+        attack_power,
+        attack_range,
+        weapon
+    ):
+        self.life = life
+        self.speed = speed
+        self.attack_power = attack_power
+        self.attack_range = attack_range
+        self.weapon = weapon
+
+    def __str__(self):
+
+        return "Life: {0}\n"\
+            "Speed: {1}\n"\
+            "Attack Power: {2}\n "\
+            "Attack Range:{3}\n "\
+            "Weapon : {4}".format(
+                self.life,
+                self.speed,
+                self.attack_power,
+                self.attack_range,
+                self.weapon
+            )
